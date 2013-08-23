f26bfea2056d44362bc57a32b37c7e130552333a
hy-byakko <hy.byakko@gmail.com>
2013-08-23 12:59:37 +0800

    Delete EF.mk
    Something is wrong about the directory name

--- _posts /EF.mk
+++
@@ -1,5 +0,0 @@
-[中文教程](http://www.cnblogs.com/yangyancheng/archive/2011/05/09/2041183.html)
-
-[DBContext](http://msdn.microsoft.com/en-us/library/system.data.entity.dbcontext.validateentity(v=vs.103).aspx)
-
-[DbEntityEntry](http://msdn.microsoft.com/en-us/library/system.data.entity.infrastructure.dbentityentry(v=vs.103).aspx)
